bc0b009bd858565680c4790e6fd27aa2c053d26a
scain-ui <scain@midland.k12.ia.us>
2025-10-17 16:44:00 +0000

    1

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-//Add code below
+const quoteLength = quote.length;
+const twentySecondLetter = quote.charAt(21);
+var output = String(quoteLength);
+output =twentySecondLetter;
+document.getElementById('result').innerHTML = output;
