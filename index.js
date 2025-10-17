3b41c31f679a60b15124782c516c78b0230becf8
scain-ui <scain@midland.k12.ia.us>
2025-10-17 17:03:21 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
+
 const quoteLength = quote.length;
 const twentySecondLetter = quote.charAt(21);
 var output = String(quoteLength);
